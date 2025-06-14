667c1b66175383834fd0f298ad7f6400cbe3d27b
viDaLost <vitaledanilov@gmail.com>
2025-06-14 14:48:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from telegram.ext import (
 )
 import asyncio
 
-TOKEN = os.getenv("BOT_TOKEN") or "7209287971:AAEUlDxd-0XwMzxpecpk8BvfhlrGkLbIqrw"
+TOKEN = os.getenv("BOT_TOKEN") or "7884985343:AAFiHFCDhEPXgaTvcbHfo-Y3DZQMRFsYpNI"
 CHANNEL_ID = os.getenv("CHANNEL_ID") or "-1001652592196"
 ADMIN_ID = int(os.getenv("ADMIN_ID") or "1288379477")
 SETTINGS_FILE = "settings.json"
